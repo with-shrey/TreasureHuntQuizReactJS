b0189118d05962873088f785fe4d08e1cb1d0053
Shrey Gupta <shrey.gupta1nov@gmail.com>
2018-10-04 01:22:47 +0530

    Removed Redundant Files

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const API_BASE = 'http://localhost/api/'
-export const LOGIN = `${API_BASE}login.php`
-export const QUESTION = `${API_BASE}question.php`
-export const SUBMIT = `${API_BASE}submit.php`
-export const LEADERBOARD = `${API_BASE}leaderboard.php`
